202c6c5a8f6daea61cd227c7150523ef3acafa59
Saksham Sethi <sakshamsethi353@gmail.com>
2026-03-27 00:08:36 +0530

    edited configure

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
-export const appConfig = {
+export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]))
