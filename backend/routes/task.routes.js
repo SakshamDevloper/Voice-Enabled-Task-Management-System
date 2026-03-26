e32a3fbcb22630a0feb3f25a2a8bac4183c29e13
Saksham Sethi <sakshamsethi353@gmail.com>
2026-03-26 20:23:34 +0530

    Connect task routes to controller

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json([
-    { _id: '1', title: 'Sample Task', status: 'todo', priority: 'med' }
-  ]);
-});
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask
+} = require('../controllers/task.controller');
+
+router.get('/', getTasks);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
 
 module.exports = router;
